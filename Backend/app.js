4c1bb96107dbe674ba931df420fb8ceb085353ff
rishabh-gupta-au13 <rgp02071993@gmail.com>
2021-06-19 01:41:13 +0530

    added stripe in backend

--- Backend/app.js
+++ Backend/app.js
@@ -10,10 +10,12 @@ app.use(cookieParser())
 const products=require('./routes/product');
 const auth=require('./routes/auth')
 const order=require('./routes/order')
+const payment=require('./routes/payment')
 
 app.use('/api/v1',products)
 app.use('/api/v1',auth)
 app.use('/api/v1',order)
+app.use('/api/v1',payment)
 
 // Middleware to haandle the error
 app.use(errorMiddleware);
---
+++ Backend/controllers/paymentController.js
@@ -0,0 +1,19 @@
+const catchAsyncError=require('../middlewares/catchAsyncError')
+const stripe=require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+// PROCESS STRIPE PAYMENT => /api/v1/payment/process
+
+exports.processPayment=catchAsyncError(async (req,res,next)=>{
+
+    const paymentIntent=await stripe.paymentIntent.create({
+        amount:req.body.amount,
+        currency:'usd',
+
+        metadata:{integration_check:'accept_a_payment'}
+    })
+
+    res.status(200).json({
+        success:true,
+        client_Secret:paymentIntent.client_Secret
+    })
+})
